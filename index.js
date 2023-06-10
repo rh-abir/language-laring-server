40534faea230fad5cbb85d7188a8ebb2531cc31d
rh-abir <rhabir.co@gmail.com>
2023-06-10 23:19:22 +0600

    get all data routes

--- index.js
+++ index.js
@@ -55,24 +55,37 @@ async function run() {
       res.send(result);
     });
 
-
-    // post class 
-    app.post('/class', async(req, res) => {
+    // post class
+    app.post("/class", async (req, res) => {
       const body = req.body;
       // console.log(body)
-      const result = await classCollection.insertOne(body)
-      res.send(result)
-    })
-
-    // get all class 
-    app.get('/class', async (req, res) => {
-      const result = await classCollection.find().toArray()
-      res.send(result)
-    })
+      const result = await classCollection.insertOne(body);
+      res.send(result);
+    });
 
 
 
+    // get all class
+    app.get("/class", async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
 
+
+
+    // get all class by user email
+    app.get("/class/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await classCollection.find(query).toArray();
+      console.log(result)
+      res.send(result);
+    });
+
+
+
+
+    
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
