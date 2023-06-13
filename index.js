cd1b6a14bc377d7996c4d7be76ff0ce53664117b
rh-abir <rhabir.co@gmail.com>
2023-06-13 10:53:47 +0600

    make payment getway

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 const morgan = require("morgan");
 const cors = require("cors");
+
+const SSLCommerzPayment = require("sslcommerz-lts");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -29,11 +31,16 @@ const client = new MongoClient(uri, {
   },
 });
 
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false; //true for live, false for sandbox
+
 async function run() {
   try {
     const usersCollection = client.db("languageDB").collection("users");
     const classCollection = client.db("languageDB").collection("class");
     const selectedCollection = client.db("languageDB").collection("selecteds");
+    const orderCollection = client.db("languageDB").collection("order");
 
     // get all user
     app.get("/users", async (req, res) => {
@@ -56,6 +63,89 @@ async function run() {
       const result = await usersCollection.updateOne(query, updateDoc, option);
       // console.log(result);
       res.send(result);
+    });
+
+    // payments
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const tran_id = new ObjectId().toString();
+
+      const data = {
+        total_amount: order.mony,
+        currency: "BDT",
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/suceess/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: "Computer.",
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: order.product.student.name,
+        cus_email: order.product.student.email,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+
+      console.log(data);
+
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
+
+        const finalOrder = {
+          ...order,
+          paidStatus: false,
+          tranjectionId: tran_id,
+        };
+
+        const result = orderCollection.insertOne(finalOrder);
+
+        console.log("Redirecting to: ", GatewayPageURL);
+      });
+
+      app.post("/payment/suceess/:tranId", async (req, res) => {
+        console.log(req.params.tranId);
+
+        const result = await orderCollection.updateOne(
+          { tranjectionId: req.params.tranId },
+          { $set: { paidStatus: true } }
+        );
+
+        if (result.modifiedCount > 0) {
+          res.redirect(
+            `http://localhost:5173/payment/success/${req.params.tranId}`
+          );
+        }
+      });
+
+      app.post("/payment/fail/:tranId", async (req, res) => {
+        const result = await orderCollection.deleteOne({
+          tranjectionId: req.params.tranId,
+        });
+        if (result.deletedCount) {
+          res.redirect(
+            `http://localhost:5173/payment/fail/${req.params.tranId}`
+          );
+        }
+      });
     });
 
     // get a users role
@@ -94,7 +184,7 @@ async function run() {
       const id = req.params.id;
       const body = req.body.status;
 
-      console.log("from backed", id, body)
+      console.log("from backed", id, body);
 
       const query = { _id: new ObjectId(id) };
       const updateDoc = {
@@ -124,7 +214,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/selects/:id", async (req, res) => {
+    app.delete("/select/:id", async (req, res) => {
       const id = req.params.id;
       console.log("please delete form database", id);
       const query = { _id: new ObjectId(id) };
